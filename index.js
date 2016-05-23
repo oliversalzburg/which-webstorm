d84fa5d0a990551885226d2de59892334ef38dfd
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-05-23 10:01:00 +0200

    [INIT]

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const Promise = require( "bluebird" );
+
+const fs         = Promise.promisifyAll( require( "fs" ) );
+const path       = require( "path" );
+const whichAsync = Promise.promisify( require( "which" ) );
+
+function findWebstorm() {
+	return whichAsync( "webstorm" )
+		.catch( () => {
+			// Not found on PATH, attempt manual lookup.
+			return findManual();
+		} );
+}
+
+function findManual() {
+	switch( process.platform ) {
+		case "win32":
+			return findManualWindows();
+
+		default:
+			throw new Error( `Platform '${process.platform}' is not supported yet.` );
+	}
+}
+
+function findManualWindows() {
+	return fs.readdirAsync( path.join( process.env[ "ProgramFiles(x86)" ], "JetBrains" ) )
+		.filter( entry => entry.match( /WebStorm/ ) )
+		.then( entries => {
+			if( !entries || !entries.length ) {
+				throw new Error( "WebStorm not found" );
+			}
+
+			return path.join( entries[ 0 ], "bin/WebStorm.exe" );
+		} );
+}
+
+if( module.parent ) {
+	module.exports = findWebstorm;
+	return;
+}
+
+findWebstorm()
+	.then( console.log )
+	.catch( console.error );
