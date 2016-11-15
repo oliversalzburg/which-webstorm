c0e1f64679f2dcc154212bc4f0c0a2144667a38b
Oliver Salzburg <oliver.salzburg@gmail.com>
2016-11-15 19:31:35 +0100

    [FEATURE] Tests

---
+++ test/test.js
@@ -0,0 +1,79 @@
+"use strict";
+
+const mocha = require( "mocha" );
+
+const after          = mocha.after;
+const before         = mocha.before;
+const chai           = require( "chai" );
+const chaiAsPromised = require( "chai-as-promised" );
+const describe       = mocha.describe;
+const expect         = require( "chai" ).expect;
+const it             = mocha.it;
+const mockFs         = require( "mock-fs" );
+
+chai.use( chaiAsPromised );
+
+describe( "which-webstorm", () => {
+	const whichWebstorm = require( ".." );
+
+	describe( "x86", () => {
+		let originalArch;
+		before( () => {
+			originalArch = process.arch;
+			Object.defineProperty( process, "arch", {
+				value : "x86"
+			} );
+		} );
+		after( () => {
+			Object.defineProperty( process, "arch", {
+				value : originalArch
+			} );
+		} );
+		it( "should return the x86 binary name", () => {
+			expect( whichWebstorm.webstormBinary() ).to.equal( "webstorm.exe" );
+		} );
+	} );
+
+	describe( "x64", () => {
+		let originalArch;
+		before( () => {
+			originalArch = process.arch;
+			Object.defineProperty( process, "arch", {
+				value : "x64"
+			} );
+		} );
+		after( () => {
+			Object.defineProperty( process, "arch", {
+				value : originalArch
+			} );
+		} );
+		it( "should return the x64 binary name", () => {
+			expect( whichWebstorm.webstormBinary() ).to.equal( "webstorm64.exe" );
+		} );
+	} );
+
+	describe( "eap directory", () => {
+		let originalArch;
+		before( () => {
+			originalArch = process.arch;
+			Object.defineProperty( process, "arch", {
+				value : "x64"
+			} );
+		} );
+		after( () => {
+			Object.defineProperty( process, "arch", {
+				value : originalArch
+			} );
+		} );
+
+		before( () => mockFs( {
+			"C:\\Program Files (x86)\\JetBrains\\WebStorm 163.9999\\bin\\webstorm64.exe" : "foo"
+		} ) );
+		after( () => mockFs.restore() );
+
+		it( "should return the webstorm binary", () => {
+			return expect( whichWebstorm() ).to.eventually.equal(
+				"C:\\Program Files (x86)\\JetBrains\\WebStorm 163.9999\\bin\\webstorm64.exe" );
+		} );
+	} );
+} );
