7f5584bb778800ea92fa3e67d3a30ad534dd8bf6
Oliver Salzburg <oliver.salzburg@gmail.com>
2020-08-14 10:44:59 +0200

    fix: wstorm binary uses Bluebird

--- bin/webstorm.js
+++ bin/webstorm.js
@@ -2,8 +2,6 @@
 
 "use strict";
 
-const Promise = require("bluebird");
-
 const execa = require("execa");
 const fs = require("fs");
 const path = require("path");
@@ -13,19 +11,21 @@ whichWebstorm().then((webstorm) => {
 	const args = process.argv.splice(2);
 
 	if (args.length) {
-		return Promise.map(args, (arg) => {
-			// Attempt to construct absolute path.
-			// This ensures that WebStorm doesn't try to resolve paths relative to
-			// the location where the webstorm binary is located.
-			const fullPath = path.resolve(arg);
-			if (fs.statSync(fullPath)) {
-				arg = fullPath;
-			}
+		return Promise.all(
+			args.map((arg) => {
+				// Attempt to construct absolute path.
+				// This ensures that WebStorm doesn't try to resolve paths relative to
+				// the location where the webstorm binary is located.
+				const fullPath = path.resolve(arg);
+				if (fs.statSync(fullPath)) {
+					arg = fullPath;
+				}
 
-			return execa(webstorm, [arg], {
-				cwd: process.cwd(),
-			});
-		});
+				return execa(webstorm, [arg], {
+					cwd: process.cwd(),
+				});
+			})
+		);
 	}
 
 	return execa(webstorm, {
