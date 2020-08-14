710fd863289c758ef8d0d23b1a2269d37668f455
Oliver Salzburg <oliver.salzburg@gmail.com>
2020-08-14 10:24:34 +0200

    feat: Rewrite as class

--- index.js
+++ index.js
@@ -7,71 +7,84 @@ const path = require("path");
 const which = require("which");
 const semver = require("semver");
 
-function findWebstorm() {
-	return which(findWebstorm.webstormBinary()).catch(() => {
-		// Not found on PATH, attempt manual lookup.
-		return findManual();
-	});
-}
-findWebstorm.webstormBinary = () =>
-	process.arch === "x64" ? "webstorm64.exe" : "webstorm.exe";
-
-function findManual() {
-	switch (process.platform) {
-		case "win32":
-			return findManualWindows();
-
-		default:
-			throw new Error(`Platform '${process.platform}' is not supported.`);
+class WebStormLocator {
+	webstormBinary() {
+		return process.arch === "x64" ? "webstorm64.exe" : "webstorm.exe";
 	}
-}
 
-function customSort(a, b) {
-	return semver.gt(a.version, b.version) ? -1 : 1;
-}
-function getLatest(entries) {
-	if (!entries || !entries.length) {
-		throw new Error("WebStorm not found");
+	async findWebstorm() {
+		try {
+			return await which(this.webstormBinary());
+		} catch (error) {
+			// Not found on PATH, attempt manual lookup.
+			return this.findManual();
+		}
 	}
-	return entries[0].path;
-}
 
-function findManualWindows() {
-	return fs.promises
-		.readdir(path.join(process.env["ProgramFiles(x86)"], "JetBrains"))
-		.then((result) =>
-			getLatest(
-				result
-					.filter((entry) => entry.match(/WebStorm/))
-					.map((entry) => {
-						return path.join(
-							process.env["ProgramFiles(x86)"],
-							"JetBrains",
-							entry,
-							"bin",
-							findWebstorm.webstormBinary()
-						);
-					})
-					.filter((candidate) =>
-						fs.promises
-							.stat(candidate)
-							.then(() => true)
-							.catch(() => false)
-					)
-					.map((entry) => {
-						const ver = entry.match(/WebStorm[^0-9]([0-9\.]+)/) || ["", ""];
-						return {
-							version: semver.valid(semver.coerce(ver[1])),
-							path: entry,
-						};
-					})
-					.sort(customSort)
-			)
-		);
+	findManual() {
+		switch (process.platform) {
+			case "win32":
+				return this.findManualWindows();
+
+			default:
+				throw new Error(`Platform '${process.platform}' is not supported.`);
+		}
+	}
+
+	findManualWindows() {
+		return fs.promises
+			.readdir(path.join(process.env["ProgramFiles(x86)"], "JetBrains"))
+			.then((result) =>
+				this._getLatest(
+					result
+						.filter((entry) => entry.match(/WebStorm/))
+						.map((entry) => {
+							return path.join(
+								process.env["ProgramFiles(x86)"],
+								"JetBrains",
+								entry,
+								"bin",
+								this.webstormBinary()
+							);
+						})
+						.filter((candidate) =>
+							fs.promises
+								.stat(candidate)
+								.then(() => true)
+								.catch(() => false)
+						)
+						.map((entry) => {
+							const ver = entry.match(/WebStorm[^0-9]([0-9\.]+)/) || ["", ""];
+							return {
+								version: semver.valid(semver.coerce(ver[1])),
+								path: entry,
+							};
+						})
+						.sort(this._customSort)
+				)
+			);
+	}
+
+	_customSort(a, b) {
+		return semver.gt(a.version, b.version) ? -1 : 1;
+	}
+
+	_getLatest(entries) {
+		if (!entries || !entries.length) {
+			throw new Error("WebStorm not found");
+		}
+		return entries[0].path;
+	}
 }
 
 if (module.parent) {
-	module.exports = findWebstorm;
+	const locator = new WebStormLocator();
+
+	// For compatibility with older versions, export a matching interface.
+	module.exports = locator.findWebstorm.bind(locator);
+	module.exports.webstormBinary = locator.webstormBinary.bind(locator);
+	// Also export the class itself for external consumption.
+	module.exports.WebStormLocator = WebStormLocator;
 } else {
-	findWebstorm().then(console.log).catch(console.error);
+	new WebStormLocator().findWebstorm().then(console.log).catch(console.error);
 }
